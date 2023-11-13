026084be79d38059030f9136db9717a3dc9a759f
lunarmoon7 <49crehbgr@gmail.com>
2023-11-13 22:41:00 +0900

    feat: creating post list - use data fetching

---
+++ app/posts/page.tsx
@@ -0,0 +1,22 @@
+import { getAllPosts } from '../libs/post';
+
+async function getProps() {
+  return {
+    props: {
+      posts: getAllPosts(),
+    },
+  };
+}
+
+export default async function PostsPage() {
+  const {
+    props: { posts },
+  } = await getProps();
+  return (
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>{post.slug}</li>
+      ))}
+    </ul>
+  );
+}
