8414763084421cae562ce182d9ed1a3a5520dc43
lunarmoon7 <49crehbgr@gmail.com>
2023-11-14 19:25:53 +0900

    refactor: change mdx to contentlayer

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -1,50 +1,57 @@
-import { serializeMdx } from "@/app/libs/mdx";
-import { getAllPosts } from "@/app/libs/posts";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import { Suspense } from "react";
-import { serialize } from "v8";
+import { format, parseISO } from 'date-fns';
+import { allPosts } from 'contentlayer/generated';
+import { getMDXComponent } from 'next-contentlayer/hooks';
 
-// Return a list of `params` to populate the [...slug] dynamic segment
-// at build time.
-export async function generateStaticParams() {
-  const posts = getAllPosts();
-  // http://localhost:3000/blog/2023/10/test 에서 동작
-  // const paths = posts.map((post) => ({
-  //   params: { slug: post.slug }
-  // }));
-  const paths = posts.map((post) => `/blog${post.slug}`); // http://localhost:3000/blog/posts/2023/10/test 에서 동작
+// similar with getStaticPaths
+export const generateStaticParams = async () => {
+  const paths = allPosts.map((post) => ({
+    params: { slug: post._raw.flattenedPath },
+  }));
   return paths;
-}
+};
 
-// Return the path of individual pages
-async function getProps({ params } : { params: { slug: string[] }}) {
-  const { slug } = params as { slug: string[]};
-  const paths = `/${[...slug].join('/')}`; // http://localhost:3000/blog/posts/2023/10/test 에서 동작
-  // const paths = `/posts/${[...slug].join('/')}`; // http://localhost:3000/blog/2023/10/test 에서 동작
-  const post = getAllPosts().find((post) => post.slug === paths);
-  if (!post) {
-    return {
-      notFound: true,
-    }
-  }
-
-  // Dosen't work anymore
-  // const mdx = await serializeMdx(post.content);
+// data fetching
+async function getProps({ params }: { params: { slug: string[] } }) {
+  const { slug } = params as { slug: string[] };
+  const post = allPosts.find(
+    (post) => post._raw.flattenedPath === slug.join('/')
+  );
   return {
-    frontMatter: post
-  }
+    props: {
+      post,
+    },
+  };
 }
 
-// params come from `generateStaticParams` above.
-export default async function PostPage({params} : { params: { slug: string[] }}) {
-  const { frontMatter } = await getProps({ params }); 
+// params came from `generateStaticParams` above.
+export default async function PostPage({
+  params,
+}: {
+  params: { slug: string[] };
+}) {
+  const {
+    props: { post },
+  } = await getProps({ params });
+
+  const MDXComponent = getMDXComponent(post!.body.code);
   return (
-    <div className="prose dark:prose-dark mt-4 w-full max-w-none">
-      <Suspense fallback={<>Loading...</>}>
-        <MDXRemote
-          source={frontMatter ? frontMatter.content: '내용이 없습니다.'}
-        />
-      </Suspense>
+    <div className='prose dark:prose-dark mt-4 w-full max-w-none'>
+      <article className='mx-auto max-w-xl py-8'>
+        <div className='mb-8 text-center'>
+          <time dateTime={post!.date} className='mb-1 text-xs text-gray-600'>
+            {format(parseISO(post!.date), 'LLLL d, yyyy')}
+          </time>
+          <div className='flex justify-center items-center'>
+            {post!.tags?.map((tag: string) => (
+              <div key={tag} className='text-xs text-gray-600 mr-2'>
+                {tag}
+              </div>
+            ))}
+          </div>
+          <h1 className='text-3xl font-bold'>{post!.title}</h1>
+          <MDXComponent />
+        </div>
+      </article>
     </div>
   );
 }
