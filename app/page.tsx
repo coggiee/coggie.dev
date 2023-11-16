aeb471cc192d28f954e75ddb81f9fded99fd5868
lunarmoon7 <49crehbgr@gmail.com>
2023-11-16 16:05:42 +0900

    feat: add components - Hero, PostSection, InfoSection, UpdateSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,37 @@
-import { RecentPosts } from './components/RecentPosts';
+import { Hero } from './components/Hero';
+import { InfoSection } from './components/InfoSection';
+import { PostSection } from './components/PostSection';
 import { allPosts } from '@/.contentlayer/generated';
+import { UpdateSection } from './components/UpdateSection';
 
 async function getProps() {
   const posts = allPosts.sort(
     (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
   );
 
+  // 읽을 만한 포스트 필터링
+  const hotPosts = posts.filter((post) => post?.hot === true);
+
   return {
     props: {
-      posts,
+      hotPosts,
     },
   };
 }
 
 export default async function Home() {
   const {
-    props: { posts },
+    props: { hotPosts },
   } = await getProps();
 
   return (
-    <div>
-      <span className='font-bold'>HOME</span>
-      <RecentPosts posts={posts} />
-    </div>
+    <section className='w-full mx-auto flex flex-col md:flex-row gap-5 relative'>
+      <div className='flex flex-col flex-grow min-w-0 w-full border-b-2 mb-3'>
+        <Hero />
+        <PostSection posts={hotPosts} title={'읽어 볼만한 포스트'} />
+        <InfoSection />
+      </div>
+      <UpdateSection />
+    </section>
   );
 }
