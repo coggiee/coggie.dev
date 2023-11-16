32f245d2e257104f1b846347569d205e28a0ee4e
lunarmoon7 <49crehbgr@gmail.com>
2023-11-16 20:59:55 +0900

    chore:

--- app/data/updateList.ts
+++ app/data/updateList.ts
@@ -5,6 +5,11 @@ interface UpdateList {
 export const updateList: UpdateList[] = [
   {title: 'tag filter'},
   {title: 'edit on page & markdown preview'},
+  {title: 'connect to db'},
   {title: 'theme switcher(darkmode)'},
   {title: 'search content'},
+  {title: 'add comment'},
+  {title: 'add playlist'},
+  {title: 'series filter'},
+  {title: 'language switcher'}
 ]
