b613d747aa53b3b58c6fa0e99b5eba1ce71980f6
lunarmoon7 <49crehbgr@gmail.com>
2023-11-16 16:02:49 +0900

    chore: add updatelist data

---
+++ app/data/updateList.ts
@@ -0,0 +1,10 @@
+interface UpdateList {
+  title: string;
+}
+
+export const updateList: UpdateList[] = [
+  {title: 'tag filter'},
+  {title: 'edit on page & markdown preview'},
+  {title: 'theme switcher(darkmode)'},
+  {title: 'search content'},
+]
