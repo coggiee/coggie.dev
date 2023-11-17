decf8d57325529605cf19e206d27a05abf52c22c
lunarmoon7 <49crehbgr@gmail.com>
2023-11-17 23:13:30 +0900

    style: change styles

--- app/components/ui/HorizontalProgress.tsx
+++ app/components/ui/HorizontalProgress.tsx
@@ -6,7 +6,7 @@ import styles from 'styles/components/HorizontalProgress.module.scss';
 const HorizontalProgress = ({ scroll }: { scroll: string }) => {
   return (
     <div className='w-full fixed top-24 left-0 h-fit z-[99]'>
-      <div className={`w-[0%] h-2 bg-[#f7ab0a]`} style={{ width: scroll }} />
+      <div className={`w-[0%] h-1 bg-[#f7ab0a]`} style={{ width: scroll }} />
     </div>
   );
 };
