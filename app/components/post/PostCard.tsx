5a81e92ce30d7ec209bb6a2c8c5e39e12b7fa471
lunarmoon7 <49crehbgr@gmail.com>
2023-11-17 22:48:01 +0900

    feat: chage into daisyUI

--- app/components/post/PostCard.tsx
+++ app/components/post/PostCard.tsx
@@ -23,25 +23,27 @@ export const PostCard = ({
   readTimeMinutes,
 }: PostCardProps) => {
   return (
-    <div className='border-b-2 border-[#f7ab0a] m-2 p-5 shadow-md rounded-lg font-mono hover:scale-105 duration-300'>
-      <Link href={`/blog/${path}`} passHref className='flex flex-col gap-2'>
-        <div className='font-medium text-xs text-gray-400 flex gap-2 items-center'>
-          <IconBxCalendarStar />
-          {formatDate(date)} /{' '}
-          {format(parseISO(date), 'cccc LLLL d, yyyy', { locale: ko })}
-        </div>
-        <div className='font-medium text-xs text-gray-400 flex gap-1 items-center'>
-          <IconTimerSand />
-          {readTimeMinutes.split(' ').slice(0, 2).join(' ')}
-        </div>
-        <div className='text-xl w-full break-words'>{title}</div>
-        <div className='font-light w-full text-ellipsis overflow-hidden whitespace-nowrap'>
-          {description}
-        </div>
-        <div className='flex gap-3 text-sm'>
-          {tags && tags.map((tag) => <Tag key={tag} tag={tag} />)}
-        </div>
-      </Link>
+    <div className='card bg-base-100 shadow-md font-mono hover:scale-105 duration-300 m-2'>
+      <div className='card-body'>
+        <Link href={`/blog/${path}`} passHref className='flex flex-col gap-2'>
+          <div className='font-medium text-xs text-gray-400 flex gap-2 items-center'>
+            <IconBxCalendarStar />
+            {formatDate(date)} /{' '}
+            {format(parseISO(date), 'cccc LLLL d, yyyy', { locale: ko })}
+          </div>
+          <div className='font-medium text-xs text-gray-400 flex gap-1 items-center'>
+            <IconTimerSand />
+            {readTimeMinutes.split(' ').slice(0, 2).join(' ')}
+          </div>
+          <h2 className='text-xl w-full break-words'>{title}</h2>
+          <div className='font-light w-full text-ellipsis overflow-hidden whitespace-nowrap'>
+            {description}
+          </div>
+          <div className='card-actions justify-start'>
+            {tags && tags.map((tag) => <Tag key={tag} tag={tag} />)}
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
