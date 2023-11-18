c304acc8f0dd488d38484775197e7c9a627d612c
lunarmoon7 <49crehbgr@gmail.com>
2023-11-18 16:04:47 +0900

    feat: add collapse

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import { Equipment } from './Equipment';
+import { Playlist } from './Playlist';
 import { Update } from './Update';
 
 export const Sidebar = () => {
   return (
-    <aside className='hidden sticky top-[120px] right-0 flex-grow-0 flex-shrink-0 w-auto h-fit basis-1/4 p-3 border-b-2 border-[#f7ab0a]/50 md:flex flex-col gap-5 items-center'>
+    <aside className='hidden sticky top-[120px] right-0 flex-grow-0 flex-shrink-0 w-auto h-fit basis-1/4 pb-5  md:flex flex-col gap-5 items-center'>
       <Update />
       <Equipment />
+      <Playlist />
     </aside>
   );
 };
