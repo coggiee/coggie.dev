4095f244e2b53598023c2b29c3f4206cd5f2ed91
lunarmoon7 <49crehbgr@gmail.com>
2023-11-18 16:38:25 +0900

    feat: add github link

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -5,9 +5,7 @@ import IconPrevBlog from '@/app/Icons/IconPrevBlog';
 export const Footer = () => {
   return (
     <footer className='w-full flex flex-col-reverse gap-2 md:flex-row justify-center items-center h-20 md:gap-5 border-t-[1px] border-t-[#dbdbdb] font-lato'>
-      <div className='text-sm'>
-        Copyright. 2023 Ⓒ Zentechie(Moon Hwisik)
-      </div>
+      <div className='text-sm'>Copyright. 2023 Ⓒ Zentechie(Moon Hwisik)</div>
       <div className='flex items-center gap-5 text-black'>
         <button className='rounded-full text-2xl'>
           <IconGithub />
