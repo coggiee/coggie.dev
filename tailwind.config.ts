c8567ea40e41ec31e8bdd8b71050bf51bc09715a
lunarmoon7 <49crehbgr@gmail.com>
2023-11-19 15:40:03 +0900

    build: add plugin - prettier for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -26,6 +26,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  darkMode: 'class',
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    'prettier-plugin-tailwindcss',
+  ],
 };
 export default config;
