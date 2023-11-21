a6ee3fc30e923cfdb10b4cf1a73f47a8755ef4f2
lunarmoon7 <49crehbgr@gmail.com>
2023-11-21 18:36:56 +0900

    feat: add Editor

---
+++ app/write/page.tsx
@@ -0,0 +1,9 @@
+import EditSection from '../components/editor/EditSection';
+
+export default function Write() {
+  return (
+    <div className='dark:text-[#fff] w-full mx-auto flex flex-col  md:flex-row gap-5 relative'>
+      <EditSection />
+    </div>
+  );
+}
