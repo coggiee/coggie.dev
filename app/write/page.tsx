2c950dfc2b61122e1fa53dd2e976500bb7d1d969
lunarmoon7 <49crehbgr@gmail.com>
2023-11-22 18:51:57 +0900

    feat: add suspense

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react';
 import EditSection from '../components/editor/EditSection';
 
 export default function Write() {
   return (
     <div className='dark:text-[#fff] w-full mx-auto flex flex-col  md:flex-row gap-5 relative'>
-      <EditSection />
+      <Suspense fallback={<div>Loading...</div>}>
+        <EditSection />
+      </Suspense>
     </div>
   );
 }
