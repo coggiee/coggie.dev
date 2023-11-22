be1fbddd420c8666c2d45f0bb6f598590cb213d0
lunarmoon7 <49crehbgr@gmail.com>
2023-11-22 18:48:16 +0900

    remove: remove contentlayer

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
-const { withContentlayer } = require('next-contentlayer');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = nextConfig;
