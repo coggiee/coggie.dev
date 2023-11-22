bc431dbb2ec376a13dc526cce97fe2f699e59f91
lunarmoon7 <49crehbgr@gmail.com>
2023-11-23 01:39:07 +0900

    chore:

--- app/data/updateList.ts
+++ app/data/updateList.ts
@@ -8,5 +8,7 @@ export const updateList: UpdateList[] = [
   {title: 'connect to db'},
   {title: 'search content'},
   {title: 'series filter'},
-  {title: 'language switcher'}
+  {title: 'language switcher'},
+  {title: 'view count'},
+  {title: 'like count'},
 ]
