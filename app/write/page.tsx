5a1f1b33e3ecc8e4bdf973fd73cfb208d4e8584e
lunarmoon7 <49crehbgr@gmail.com>
2023-11-22 19:05:25 +0900

    fix: fix navigator is not defined

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,11 +1,19 @@
 import { Suspense } from 'react';
 import EditSection from '../components/editor/EditSection';
+import dynamic from 'next/dynamic';
+
+const DynamicEditSection = dynamic(
+  () => import('../components/editor/EditSection'),
+  {
+    ssr: false,
+  }
+);
 
 export default function Write() {
   return (
     <div className='dark:text-[#fff] w-full mx-auto flex flex-col  md:flex-row gap-5 relative'>
       <Suspense fallback={<div>Loading...</div>}>
-        <EditSection />
+        <DynamicEditSection />
       </Suspense>
     </div>
   );
