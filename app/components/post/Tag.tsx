877409ce6d8b007e57ea06e4b73120cff8475600
lunarmoon7 <49crehbgr@gmail.com>
2023-11-23 00:27:34 +0900

    style: change styles

--- app/components/post/Tag.tsx
+++ app/components/post/Tag.tsx
@@ -1,3 +1,5 @@
 export const Tag = ({ tag }: { tag: string }) => {
-  return <div className='badge badge-neutral cursor-pointer rounded-lg py-3 dark:bg-[#c1c1c12f] dark:text-white  border-none'>{tag}</div>;
+  return <div className='badge badge-neutral cursor-pointer rounded-lg py-3 dark:bg-[#c1c1c12f] dark:text-white  border-none flex items-center'>
+    <span>{tag}</span>
+  </div>;
 };
