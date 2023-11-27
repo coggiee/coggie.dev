433572f60d40a853f0cc641b5482521b200cb634
lunarmoon7 <49crehbgr@gmail.com>
2023-11-27 23:02:49 +0900

    style: style 수정

--- app/_components/ui/Tag.tsx
+++ app/_components/ui/Tag.tsx
@@ -1,5 +1,5 @@
 export const Tag = ({ tag }: { tag: string }) => {
-  return <div className='badge border border-[#7e7e7ea8] cursor-pointer rounded-lg py-3 dark:bg-[#c1c1c12f] dark:text-white flex items-center'>
+  return <div className='badge border border-[#7e7e7ea8] cursor-pointer rounded-lg py-3 dark:bg-[#c1c1c12f] dark:text-white flex items-center sm:self-end'>
     <span># {tag}</span>
   </div>;
 };
