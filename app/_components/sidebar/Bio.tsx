1e0bfbd766650eab602bf508a87f7a1a7e9729a4
lunarmoon7 <49crehbgr@gmail.com>
2023-12-09 15:51:28 +0900

    style: style 수정

--- app/_components/sidebar/Bio.tsx
+++ app/_components/sidebar/Bio.tsx
@@ -7,7 +7,7 @@ export default function Bio({}: Props) {
   return (
     <div className='border-b border-b-item-border-light pb-5 dark:border-b-item-border-dark'>
       <h1 className='font-bold mb-3 text-xl'>Bio</h1>
-      <ul className='font-thin text-sm'>
+      <ul className='font-thin text-sm flex flex-col gap-2'>
         {bio.map((item) => (
           <li className='flex gap-2' key={item.title}>
             <span>{item.duration}</span>
