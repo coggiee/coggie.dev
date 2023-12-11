93d9142f2c33d3d8fd03661301ba0198f1202f33
lunarmoon7 <49crehbgr@gmail.com>
2023-12-11 17:59:48 +0900

    fix: 전체 화면으로 포스트 볼 때 링크 복사 수정 - 같은 주소가 2번 들어가는 문제 수정

--- utils/copyToClipboard.ts
+++ utils/copyToClipboard.ts
@@ -1,4 +1,9 @@
 export const copyToClipboard = (path: string) => {
-  const currentPath = window.location.toString() + `blog/${path}`;
-  navigator.clipboard.writeText(currentPath);
+  const currentPath = window.location.toString();
+  if (currentPath.includes('/blog')) {
+    navigator.clipboard.writeText(currentPath);
+  } else {
+    const currentPath = window.location.toString() + `blog/${path}`;
+    navigator.clipboard.writeText(currentPath);
+  }
 };
