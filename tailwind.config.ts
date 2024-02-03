8ef857b13370368ae8c3309658527d08986da491
lunarmoon7 <zentechie7@gmail.com>
2024-02-03 22:29:42 +0900

    chore: nextUI 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'tailwindcss';
 const { spacing } = require('tailwindcss/defaultTheme');
+const {nextui} = require("@nextui-org/react")
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -60,6 +62,7 @@ const config: Config = {
     require('daisyui'),
     'prettier-plugin-tailwindcss',
     require('tailwind-scrollbar'),
+    nextui(),
   ],
 };
 export default config;
