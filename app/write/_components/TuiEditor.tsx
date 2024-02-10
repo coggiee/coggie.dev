fb6c10cff3e067896dae7f6ede641d399cb49caf
lunarmoon7 <zentechie7@gmail.com>
2024-02-10 18:14:53 +0900

    refactor: 이미지 업로드 함수 수정
    - 드래그 앤 드롭도 가능하도록 useEffect가 아닌 함수로 등록

--- app/write/_components/TuiEditor.tsx
+++ app/write/_components/TuiEditor.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import { Editor } from "@toast-ui/react-editor";
 import codeSyntaxHighlight from "@toast-ui/editor-plugin-code-syntax-highlight";
 import "@toast-ui/editor/dist/toastui-editor.css";
@@ -14,6 +14,7 @@ import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 import { EditorProps } from "@/types/type";
 import { useTheme } from "next-themes";
 import { useResize } from "@/app/_hooks/useResize";
+import UploadLoading from "./UploadLoading";
 
 export default function TuiEditor({ content = "", editorRef }: EditorProps) {
   const { direction } = useResize();
@@ -22,37 +23,29 @@ export default function TuiEditor({ content = "", editorRef }: EditorProps) {
     ["code", "codeblock"],
     ["image"],
   ];
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
   const theme = useTheme().theme;
 
-  useEffect(() => {
-    if (editorRef.current) {
-      editorRef.current.getInstance().removeHook("addImageBlobHook");
-      editorRef.current
-        .getInstance()
-        .addHook("addImageBlobHook", (file: File | Blob, callback: any) => {
-          (async () => {
-            const form = new FormData();
-            form.append("fileUpload", file);
+  const handleAddImageBlob = async (file: File | Blob, callback: any) => {
+    setIsLoading(true);
+    const form = new FormData();
+    form.append("fileUpload", file);
 
-            const response = await fetch("/api/upload", {
-              method: "POST",
-              body: form,
-            });
+    const response = await fetch("/api/upload", {
+      method: "POST",
+      body: form,
+    });
 
-            const { data } = await response.json();
-            const { url } = data;
-            callback(url, "image");
-          })();
-
-          return false;
-        });
-    }
-
-    return () => {};
-  }, [editorRef]);
+    const { data } = await response.json();
+    const { url } = data;
+    callback(url, "image");
+    setIsLoading(false);
+    return false;
+  };
 
   return (
-    <div className="flex-grow">
+    <div className="flex-grow relative">
       {editorRef && (
         <div className="flex flex-col gap-3 mb-3">
           <Editor
@@ -66,6 +59,9 @@ export default function TuiEditor({ content = "", editorRef }: EditorProps) {
             initialValue={" "}
             useCommandShortcut={true}
             toolbarItems={toolbarItems}
+            hooks={{
+              addImageBlobHook: handleAddImageBlob,
+            }}
             plugins={[
               [
                 codeSyntaxHighlight,
@@ -78,6 +74,7 @@ export default function TuiEditor({ content = "", editorRef }: EditorProps) {
           />
         </div>
       )}
+      {isLoading && <UploadLoading />}
     </div>
   );
 }
