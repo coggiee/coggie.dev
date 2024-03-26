0231cfde5490b04a545108966357bb434d6f8f27
lunarmoon7 <zentechie7@gmail.com>
2024-03-26 22:18:40 +0900

    chore: 사용하지 않는 라이브러리 삭제

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,65 +1,64 @@
-import type { Config } from 'tailwindcss';
-const { spacing } = require('tailwindcss/defaultTheme');
-const {nextui} = require("@nextui-org/react")
+import type { Config } from "tailwindcss";
+const { spacing } = require("tailwindcss/defaultTheme");
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        inter: ['var(--font-inter)'],
-        blackHanSans: ['var(--font-blackHanSans)'],
-        notosanskr: ['var(--font-notosanskr)'],
-        lato: ['var(--font-lato)'],
-        marhey: ['var(--font-marhey)'],
-        pacifico: ['var(--font-pacifico)'],
-        indieFlower: ['var(--font-indieFlower)'],
-        permanentMarker: ['var(--font-permanentMarker)'],
-        dhurjati: ['var(--font-dhurjati)'],
-        teko: ['var(--font-teko)'],
+        inter: ["var(--font-inter)"],
+        blackHanSans: ["var(--font-blackHanSans)"],
+        notosanskr: ["var(--font-notosanskr)"],
+        lato: ["var(--font-lato)"],
+        marhey: ["var(--font-marhey)"],
+        pacifico: ["var(--font-pacifico)"],
+        indieFlower: ["var(--font-indieFlower)"],
+        permanentMarker: ["var(--font-permanentMarker)"],
+        dhurjati: ["var(--font-dhurjati)"],
+        teko: ["var(--font-teko)"],
       },
       transitionProperty: {
-        'max-height': 'max-height',
+        "max-height": "max-height",
       },
       typography: (theme: any) => ({
         DEFAULT: {
           css: {
             //...
-            'h1,h2,h3,h4': {
-              'scroll-margin-top': spacing[32],
+            "h1,h2,h3,h4": {
+              "scroll-margin-top": spacing[32],
             },
           },
         },
       }),
       colors: {
-        'item-light': '#f8f8f898',
-        'hover-light': '#99999923',
-        'hover-dark': '#434343',
-        'item-border-light': '#dcdcdc',
-        'item-dark': '#1c1c1c',
-        'item-border-dark': '#353535',
-        'brand-color': 'dodgerblue',
-        'sub-light': '#ffffff8d',
-        'sub-dark': '#3e3e3e',
+        "item-light": "#f8f8f898",
+        "hover-light": "#99999923",
+        "hover-dark": "#434343",
+        "item-border-light": "#dcdcdc",
+        "item-dark": "#1c1c1c",
+        "item-border-dark": "#353535",
+        "brand-color": "dodgerblue",
+        "sub-light": "#ffffff8d",
+        "sub-dark": "#3e3e3e",
       },
     },
   },
-  darkMode: 'class',
+  darkMode: "class",
   plugins: [
-    require('@tailwindcss/typography'),
-    require('daisyui'),
-    'prettier-plugin-tailwindcss',
-    require('tailwind-scrollbar'),
+    require("@tailwindcss/typography"),
+    "prettier-plugin-tailwindcss",
+    require("tailwind-scrollbar"),
     nextui(),
   ],
 };
