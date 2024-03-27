06fc4c106baf7fd225c0165376c528e41b414fc7
lunarmoon7 <zentechie7@gmail.com>
2024-03-27 17:55:24 +0900

    feat: 포스트 없을 경우 예외 처리

--- app/_components/section/AsidePostList.tsx
+++ app/_components/section/AsidePostList.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, CardHeader } from "@nextui-org/react";
 import Link from "next/link";
 import React from "react";
+import Fallback from "../common/Fallback";
 
 interface PostListProps {
   posts: any;
@@ -12,26 +13,28 @@ export default function AsidePostList({ posts, title }: PostListProps) {
     <aside className="space-y-5 w-full">
       <h1 className="text-lg font-semibold dark:text-white w-full">{title}</h1>
       <main className="flex flex-col gap-2">
-        {posts.map(({ node }: { node: any }) => (
-          <Link
-            key={node.id}
-            href={`/blog/${node.id}`}
-            passHref
-            className="hover:scale-105 transition-all duration-250"
-          >
-            <Card
-              isBlurred
-              className="w-full rounded-lg font-mono bg-item-light dark:bg-item-dark"
+        {posts.length === 0 && <Fallback title={"아직 포스트가 없습니다."} />}
+        {posts.length > 0 &&
+          posts.map(({ node }: { node: any }) => (
+            <Link
+              key={node.id}
+              href={`/blog/${node.id}`}
+              passHref
+              className="hover:scale-105 transition-all duration-250"
             >
-              <CardHeader>
-                <h2 className="text-sm w-full truncate">{node.title}</h2>
-              </CardHeader>
-              <CardBody className="text-xs truncate text-gray-700 dark:text-gray-300 py-0 pb-3">
-                {node.description}
-              </CardBody>
-            </Card>
-          </Link>
-        ))}
+              <Card
+                isBlurred
+                className="w-full rounded-lg font-mono bg-item-light dark:bg-item-dark"
+              >
+                <CardHeader>
+                  <h2 className="text-sm w-full truncate">{node.title}</h2>
+                </CardHeader>
+                <CardBody className="text-xs truncate text-gray-700 dark:text-gray-300 py-0 pb-3">
+                  {node.description}
+                </CardBody>
+              </Card>
+            </Link>
+          ))}
       </main>
     </aside>
   );
