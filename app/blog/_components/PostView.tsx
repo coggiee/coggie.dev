7bd0e52756a9df8cc62c27ca7893b72b02529510
lunarmoon7 <zentechie7@gmail.com>
2024-03-31 21:53:28 +0900

    style: 포스트 없을 경우 스타일 추가

--- app/blog/_components/PostView.tsx
+++ app/blog/_components/PostView.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/utils/formatTime";
 import PostSideCard from "./PostSideCard";
 import Link from "next/link";
-import { Button, ScrollShadow } from "@nextui-org/react";
+import { Button, Card, CardBody, ScrollShadow } from "@nextui-org/react";
 
 interface PostViewProps {
   postList: any;
@@ -27,41 +27,46 @@ export default function PostView({
       <h1 className="text-lg font-semibold dark:text-white min-w-fit">
         {title}
       </h1>
-      <ScrollShadow
-        size={100}
-        className="flex flex-col overflow-y-scroll overscroll-y-none  max-h-[900px]"
-      >
-        <main className="flex flex-col gap-2">
-          {postList.length === 0 && (
-            <Fallback title={"아직 포스트가 없습니다."} />
-          )}
-          {postList.map((post: any) => (
-            <Link href={`/blog/${post.id}`} passHref key={post.id}>
-              <PostSideCard
-                key={post.id}
-                date={formatCreatedAt(post.date)}
-                time={formatCreatedTime(post.date)}
-                title={post.title}
-                description={post.description}
-                tags={post.tags}
-                coverImage={post.coverImage}
-                readTimeMinutes={formatReadingMinutes(post.content)}
-              />
-            </Link>
-          ))}
-          <footer className="flex justify-center items-center">
-            <Button
-              onClick={handleLoad}
-              isLoading={isLoading}
-              className="w-fit"
-              color="warning"
-              variant="flat"
-            >
-              Load
-            </Button>
-          </footer>
-        </main>
-      </ScrollShadow>
+      {postList.length === 0 ? (
+        <Card>
+          <CardBody>
+            <p className="text-[#929292]">딱히 포스트가 없네요.</p>
+          </CardBody>
+        </Card>
+      ) : (
+        <ScrollShadow
+          size={100}
+          className="flex flex-col overflow-y-scroll overscroll-y-none max-h-[900px]"
+        >
+          <main className="flex flex-col gap-2">
+            {postList.map((post: any) => (
+              <Link href={`/blog/${post.id}`} passHref key={post.id}>
+                <PostSideCard
+                  key={post.id}
+                  date={formatCreatedAt(post.date)}
+                  time={formatCreatedTime(post.date)}
+                  title={post.title}
+                  description={post.description}
+                  tags={post.tags}
+                  coverImage={post.coverImage}
+                  readTimeMinutes={formatReadingMinutes(post.content)}
+                />
+              </Link>
+            ))}
+            <footer className="flex justify-center items-center">
+              <Button
+                onClick={handleLoad}
+                isLoading={isLoading}
+                className="w-fit"
+                color="warning"
+                variant="flat"
+              >
+                Load
+              </Button>
+            </footer>
+          </main>
+        </ScrollShadow>
+      )}
     </aside>
   );
 }
