a9c7c74b3e240f1b7740b7c1e24a9289d1c48b38
lunarmoon7 <zentechie7@gmail.com>
2024-04-01 21:59:52 +0900

    refactor: form 관리 로직을 커스텀 훅으로 분리

---
+++ app/_hooks/useForm.ts
@@ -0,0 +1,191 @@
+import React, { ChangeEvent, KeyboardEvent, useRef, useState } from "react";
+import { useFormStore } from "../_store/useFormStore";
+import dayjs from "dayjs";
+import { createPost, updatePost } from "../_libs/hygraph";
+import { useRouter } from "next/navigation";
+
+interface useFormProps {
+  ref: React.MutableRefObject<any>;
+}
+export const useForm = () => {
+  const ref = useRef<any>(null);
+  const { form, isUpdated, setForm } = useFormStore();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isToast, setIsToast] = useState(false);
+  const [isUpdatedCoverImage, setIsUpdatedCoverImage] = useState(false);
+
+  const handleSubmit = async () => {
+    if (!ref) return;
+
+    setIsLoading(true);
+
+    const content: string = ref.current.getInstance().getMarkdown();
+    const {
+      id: postId,
+      title,
+      description,
+      coverImage,
+      isPinned,
+      tagList,
+    } = form;
+
+    tagList.forEach((tag) => tag.toUpperCase());
+
+    if (
+      content.trim().length === 0 ||
+      title.trim().length === 0 ||
+      tagList.length === 0 ||
+      !coverImage ||
+      !coverImage?.id
+    ) {
+      setIsLoading(false);
+      setIsToast(true);
+      setTimeout(() => {
+        setIsToast(false);
+      }, 3000);
+      return;
+    }
+
+    if (title.endsWith(".")) {
+      setForm({
+        ...form,
+        title: title.slice(0, title.length - 1),
+      });
+    }
+
+    const currentDate = dayjs().format();
+
+    let response = null;
+    let coverImageId = null;
+    const isCoverImageChanged = isUpdated ? isUpdatedCoverImage : true;
+
+    if (isCoverImageChanged) {
+      const uploadForm = new FormData();
+
+      uploadForm.append("fileUpload", coverImage);
+
+      const assetResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_HYGRAPH_URL}`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_ASSET_TOKEN}`,
+          },
+          body: uploadForm,
+        },
+      );
+
+      const { id } = await assetResponse.json();
+      coverImageId = id;
+    }
+
+    if (!isUpdated) {
+      response = await createPost(
+        title,
+        description,
+        content,
+        tagList,
+        isPinned,
+        new Date(currentDate),
+        coverImageId,
+      );
+    } else {
+      response = await updatePost(
+        isCoverImageChanged ? coverImageId : coverImage.id,
+        content,
+        description,
+        isPinned,
+        tagList,
+        title,
+        postId,
+      );
+    }
+
+    const { id } = response;
+
+    if (id) {
+      setIsLoading(false);
+      useFormStore.persist.clearStorage();
+      router.push(`/blog/${id}`);
+    }
+  };
+
+  const handleAddTag = () => {
+    const currentTags = form.tagInput.trim();
+    if (currentTags !== "") {
+      setForm({
+        ...form,
+        tagList: [...form.tagList, currentTags],
+        tagInput: "",
+      });
+    }
+  };
+
+  const handleChangeTag = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      tagInput: e.target.value,
+    });
+  };
+
+  const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      title: e.target.value,
+    });
+  };
+
+  const handleChangeDesc = (e: ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      description: e.target.value,
+    });
+  };
+
+  const handleChangeCoverImage = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files![0];
+    setIsUpdatedCoverImage(true);
+    setForm({
+      ...form,
+      coverImage: file,
+    });
+  };
+
+  const handleTogglePin = () => {
+    setForm({
+      ...form,
+      isPinned: !form.isPinned,
+    });
+  };
+
+  const handlePressEnter = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") handleAddTag();
+  };
+
+  const handleSelectTag = (selectedTag: string) => {
+    setForm({
+      ...form,
+      tagList: form.tagList.filter((tag) => tag !== selectedTag),
+    });
+  };
+
+  const handleBack = () => {
+    router.back();
+  };
+
+  return {
+    ref,
+    isLoading,
+    isToast,
+    handleSubmit,
+    handleChangeTitle,
+    handleChangeDesc,
+    handleChangeCoverImage,
+    handleChangeTag,
+    handleTogglePin,
+    handlePressEnter,
+    handleSelectTag,
+    handleBack,
+  };
+};
--- app/write/_components/WriteSection.tsx
+++ app/write/_components/WriteSection.tsx
@@ -12,164 +12,48 @@ import { Button, Chip, useDisclosure } from "@nextui-org/react";
 import Loading from "@/app/loading";
 import TitleInput from "@/app/write/_components/TitleInput";
 import TagInput from "./TagInput";
+import { useFormStore } from "@/app/_store/useFormStore";
+import { useForm } from "@/app/_hooks/useForm";
 
-interface WriteSectionProps {
-  post?: any;
-}
+export default function WriteSection() {
+  const {
+    form: {
+      id: postId,
+      title,
+      content,
+      description,
+      coverImage,
+      isPinned,
+      tagList,
+      tagInput,
+    },
+    isUpdated,
+  } = useFormStore();
 
-export default function WriteSection({ post }: WriteSectionProps) {
-  const searchParams = useSearchParams();
-  const editorRef = useRef<any>(null);
-  const [title, setTitle] = useState<string>(post ? post.title : "");
-  const [description, setDescription] = useState<string>(
-    post ? post.description : "",
-  );
-  const [coverImage, setCoverImage] = useState<any>(
-    post ? post.coverImage.url : null,
-  );
+  const {
+    ref,
+    isLoading,
+    isToast,
+    handleSubmit,
+    handleChangeTitle,
+    handleChangeDesc,
+    handleChangeCoverImage,
+    handleChangeTag,
+    handleTogglePin,
+    handlePressEnter,
+    handleSelectTag,
+    handleBack,
+  } = useForm();
 
-  const [isAlertVisible, setIsAlertVisible] = useState<boolean>(false);
-  const [isPostCreated, setIsPostCreated] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isHotPost, setIsHotPost] = useState<boolean>(post ? post.hot : false);
-  const [tags, setTags] = useState("");
-  const [tagList, setTagList] = useState<string[]>(post ? post.tags : []);
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
-
-  const router = useRouter();
-  const isUpdate = searchParams.has("isUpdate")
-    ? searchParams.get("isUpdate")
-    : false;
-
-  const handleOnSave = async () => {
-    setIsLoading(true);
-    const content: string = editorRef.current.getInstance().getMarkdown();
-    tagList.forEach((tag) => tag.toUpperCase());
-
-    // 제목과 내용이 없는 예외처리
-    if (
-      content.trim().length === 0 ||
-      title.trim().length === 0 ||
-      tagList.length === 0
-    ) {
-      setIsLoading(false);
-      setIsAlertVisible(true);
-      setTimeout(() => {
-        setIsAlertVisible(false);
-      }, 3000);
-      return;
-    }
-
-    if (title.endsWith(".")) {
-      setTitle(title.slice(0, title.length - 1));
-    }
-
-    const date = dayjs().format();
-
-    let response = null;
-    let coverImageId = null;
-    const isCoverImageChanged = post
-      ? coverImage !== post.coverImage.url
-      : true;
-
-    if (isCoverImageChanged) {
-      const form = new FormData();
-
-      form.append("fileUpload", coverImage);
-
-      const assetResponse = await fetch(
-        `${process.env.NEXT_PUBLIC_HYGRAPH_URL}`,
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_ASSET_TOKEN}`,
-          },
-          body: form,
-        },
-      );
-
-      const { id } = await assetResponse.json();
-      coverImageId = id;
-    }
-    if (!isUpdate) {
-      response = await createPost(
-        title,
-        description,
-        content,
-        tagList,
-        isHotPost,
-        new Date(date),
-        coverImageId,
-      );
-    } else {
-      response = await updatePost(
-        isCoverImageChanged ? coverImageId : post.coverImage.id,
-        content,
-        description,
-        isHotPost,
-        tagList,
-        title,
-        post.id,
-      );
-    }
-
-    const { id } = response;
-
-    if (id) {
-      setIsLoading(false);
-      setIsPostCreated(true);
-      router.push(`/blog/${id}`);
-    }
-  };
-
-  const addTags = () => {
-    if (tags.trim() !== "") {
-      setTagList([...tagList, tags.trim()]);
-      setTags("");
-    }
-  };
-
-  const handleOnTypeTitle = (value: string) => {
-    setTitle((prev) => value);
-  };
-
-  const handleOnTypeTags = (e: ChangeEvent<HTMLInputElement>) => {
-    setTags(e.target.value);
-  };
-  const handleOnTypeDesc = (e: ChangeEvent<HTMLInputElement>) => {
-    setDescription(e.target.value);
-  };
-
-  const handleOnToggleHotPost = () => {
-    setIsHotPost((prev) => !prev);
-  };
-
-  const handleKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      addTags();
-    }
-  };
-
-  const handleOnFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files![0];
-    setCoverImage(file);
-  };
-
-  const handleOnClickTag = (tagToRemove: string) => {
-    setTagList(tagList.filter((tag) => tag !== tagToRemove));
-  };
-
-  const handleBack = () => {
-    router.back();
-  };
 
   return (
     <div className="flex flex-col flex-grow flex-3 relative w-full pt-10">
-      <TitleInput title={title} handleOnTypeTitle={handleOnTypeTitle} />
+      <TitleInput title={title} handleOnTypeTitle={handleChangeTitle} />
       <TagInput
-        tags={tags}
-        handleKeyPress={handleKeyPress}
-        handleOnTypeTags={handleOnTypeTags}
+        tags={tagInput}
+        handleKeyPress={handlePressEnter}
+        handleOnTypeTags={handleChangeTag}
       />
       <div className="mb-4">
         <ul className="flex flex-wrap justify-start items-center gap-2">
@@ -180,15 +64,15 @@ export default function WriteSection({ post }: WriteSectionProps) {
               variant="solid"
               color="success"
               className="cursor-pointer"
-              onClose={() => handleOnClickTag(tag)}
+              onClose={() => handleSelectTag(tag)}
             >
               {tag}
             </Chip>
           ))}
         </ul>
       </div>
-      <TuiEditor content={post ? post.content : " "} editorRef={editorRef} />
-      <div className="w-full flex gap-3 fixed bottom-0 px-10 py-3 justify-end">
+      <TuiEditor content={isUpdated ? content : " "} editorRef={ref} />
+      <div className="flex gap-3 py-3 justify-end">
         <Button onPress={handleBack}>
           <span>나가기</span>
         </Button>
@@ -198,20 +82,17 @@ export default function WriteSection({ post }: WriteSectionProps) {
         <ReCheckModal
           isOpen={isOpen}
           onOpenChange={onOpenChange}
-          handleOnTypeDesc={handleOnTypeDesc}
-          handleOnToggleHotPost={handleOnToggleHotPost}
-          handleOnClickSaveBtn={handleOnSave}
-          handleOnFileChange={handleOnFileChange}
+          handleOnTypeDesc={handleChangeDesc}
+          handleOnToggleHotPost={handleTogglePin}
+          handleOnClickSaveBtn={handleSubmit}
+          handleOnFileChange={handleChangeCoverImage}
           description={description}
-          coverImageUrl={post ? post.coverImage.url : null}
-          defaultSelected={isHotPost}
+          coverImageUrl={coverImage ? coverImage.url : null}
+          defaultSelected={isPinned}
         />
       </div>
-      {isAlertVisible && (
+      {isToast && (
         <Alert title="제목과 내용, 태그를 확인해주세요." bgColor="crimson" />
-      )}
-      {isPostCreated && (
-        <Alert title="글이 작성되었습니다." bgColor="crimson" />
       )}
       {isLoading && <Loading />}
     </div>
