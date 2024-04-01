c71d19ca7fdc39798199c146b5796a018e4b6672
lunarmoon7 <zentechie7@gmail.com>
2024-04-01 22:43:11 +0900

    chore: 프로젝트 아이템 추가

--- app/_data/project.ts
+++ app/_data/project.ts
@@ -7,27 +7,28 @@ interface Project {
 
 export const Project: Project[] = [
   {
-    title: 'Buddies',
-    description: 'A Customized AI Chatbot Service',
-    githubLink: 'https://github.com/lunarmoon7/Buddies-NextJS',
-    demoLink: '',
+    title: "MogakGO",
+    description:
+      "Region-based Mogakko Matching Service with Gamification Elements",
+    githubLink: "https://github.com/Open-Eye-Im-Developer",
+    demoLink: "https://mogak-go.vercel.app/",
   },
   {
-    title: 'Nution-Notion Clone',
-    description: 'A Notion Clone with VanillaJS',
-    githubLink: 'https://github.com/lunarmoon7/Nution-Notion_Cloning',
-    demoLink: 'https://zentechie-nution.netlify.app/',
+    title: "Buddies",
+    description: "A Customized AI Chatbot Service",
+    githubLink: "https://github.com/lunarmoon7/Buddies-NextJS",
+    demoLink: "",
   },
   {
-    title: 'Weather App',
-    description: 'A Weather App with NextJS',
-    githubLink: 'https://github.com/lunarmoon7/weather-nextjs',
-    demoLink: 'https://weather-nextjs.pages.dev/',
+    title: "Weather App",
+    description: "A Weather App with NextJS",
+    githubLink: "https://github.com/lunarmoon7/weather-nextjs",
+    demoLink: "https://weather-nextjs.pages.dev/",
   },
   {
-    title: 'Dev Blog',
-    description: 'A Dev Blog with NextJS, MDX, GraphQL',
-    githubLink: 'https://github.com/lunarmoon7/zentechie-blog',
-    demoLink: 'https://coggie.dev',
+    title: "Dev Blog",
+    description: "A Dev Blog with NextJS, MDX, GraphQL",
+    githubLink: "https://github.com/lunarmoon7/zentechie-blog",
+    demoLink: "https://coggie.dev",
   },
 ];
