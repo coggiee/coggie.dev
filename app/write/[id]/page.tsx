191310fed747b7a488dc67606b03973eca73d70a
lunarmoon7 <zentechie7@gmail.com>
2024-04-01 21:58:37 +0900

    refactor: server fetch 제거
    - formStore로 포스트 정보를 불러오도록 변경함에 따라 server fetch가 불필요해졌습니다.

--- app/write/[id]/page.tsx
+++ app/write/[id]/page.tsx
@@ -10,23 +10,15 @@ const DynamicEditSection = dynamic(
   },
 );
 
-async function getCurrentPost(postId: string) {
-  const post = await getSinglePost(postId);
-  return {
-    post,
-  };
-}
-
 export default async function WritePage({
   params,
 }: {
   params: { id: string };
 }) {
-  const { post } = await getCurrentPost(params.id);
   return (
     <div className="dark:text-[#fff] w-full mx-auto flex flex-col  md:flex-row gap-5 relative">
       <Suspense fallback={<div>Loading...</div>}>
-        <DynamicEditSection post={post} />
+        <DynamicEditSection />
       </Suspense>
     </div>
   );
