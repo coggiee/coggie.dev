d6b31757e4fd119e9f2f69a7ff680f32bcda5f0d
lunarmoon7 <zentechie7@gmail.com>
2024-04-15 14:15:49 +0900

    cleanup: next-sitemap 관련 파일 삭제

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://coggie.dev",
-  changefreq: "daily",
-  priority: 0.7,
-  sitemapSize: 7000,
-  generateRobotsTxt: true,
-  exclude: [],
-};
