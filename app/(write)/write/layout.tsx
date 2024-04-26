c1c1d8280b263fbb3e6fde8533fc4448557b1c23
lunarmoon7 <zentechie7@gmail.com>
2024-04-26 10:57:03 +0900

    refactor: MDXEditor 적용
    - ToastUIEditor -> MDXEditor로 변경했습니다.

--- app/(write)/write/layout.tsx
+++ app/(write)/write/layout.tsx
@@ -4,7 +4,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className=" w-full min-h-screen ">
+    <div className=" w-full min-h-screen">
       {/* <main className="flex-grow flex flex-col items-center w-full px-5 md:px-5 md:flex-row md:items-baseline font-notosanskr"> */}
       {children}
       {/* </main> */}
