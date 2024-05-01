e80b10bfc39a84381c65729a361c9f0e32ecf0d2
lunarmoon7 <zentechie7@gmail.com>
2024-05-01 22:00:06 +0900

    style: 로딩 스타일 변경

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from "lucide-react";
 export default function Loading() {
   return (
     <div className="w-full h-full fixed z-[50] flex flex-col justify-center items-center">
-      <LoaderCircle className="animate-spinner-ease-spin w-7 h-7" />
+      <LoaderCircle className="animate-spin w-7 h-7" />
     </div>
   );
 }
