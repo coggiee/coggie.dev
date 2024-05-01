38d1b9874356cb97faf04c3f2011a2782e1e87e7
lunarmoon7 <zentechie7@gmail.com>
2024-05-01 21:55:59 +0900

    style: 메뉴바 스타일 변경

--- components/global/Nav.tsx
+++ components/global/Nav.tsx
@@ -37,38 +37,36 @@ export default function Nav() {
               <MenubarTrigger className="p-1 cursor-pointer">
                 <Menu />
               </MenubarTrigger>
-              <MenubarContent>
+              <MenubarContent className="font-aritaburi">
                 <MenubarItem>
                   <Link
                     href="https://music.apple.com/kr/playlist/%EB%A7%9B%EB%8F%84%EB%A6%AC%EC%97%90%EC%9A%94/pl.u-06oxp93CYpLxloY"
-                    className="flex items-center gap-3"
+                    className="flex items-center gap-2"
                   >
                     <Music className="w-4 h-4" />
                     <span>Playlist</span>
                   </Link>
                 </MenubarItem>
-                <MenubarItem className="cursor-pointer">
+                <MenubarItem className="cursor-pointer" disabled>
                   <p className="flex items-center gap-2">
                     <Languages className="w-4 h-4" />
-                    Language
+                    <span>Language</span>
                   </p>
-                  <MenubarShortcut>⌘N</MenubarShortcut>
                 </MenubarItem>
                 <MenubarSeparator />
                 {session && session.user!.email === "zentechie7@gmail.com" && (
                   <MenubarItem className="cursor-pointer">
-                    <Link href="/write" className="flex items-center gap-3">
-                      <NotebookPen />
+                    <Link href="/write" className="flex items-center gap-2">
+                      <NotebookPen className="w-4 h-4" />
                       <span>Write Post</span>
                     </Link>
                   </MenubarItem>
                 )}
                 <MenubarItem onClick={session ? handleOnLogout : handleOnLogin}>
                   <p className="flex items-center gap-2">
-                    <ScanFace className="w-4 h-4" />{" "}
-                    {session ? "Logout" : "Login"}
+                    <ScanFace className="w-4 h-4" />
+                    <span>{session ? "Logout" : "Login"}</span>
                   </p>
-                  <MenubarShortcut>⌘⇧L</MenubarShortcut>
                 </MenubarItem>
               </MenubarContent>
             </MenubarMenu>
