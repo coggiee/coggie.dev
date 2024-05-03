eb6afaa8dcc581e4a0f86579f4b4898a7ef21cb1
lunarmoon7 <zentechie7@gmail.com>
2024-05-03 15:35:10 +0900

    style: 스타일 변경

--- components/global/Introduction.tsx
+++ components/global/Introduction.tsx
@@ -6,12 +6,10 @@ export default function Introduction() {
       <aside className="text-7xl dark:text-white px-8 pr-5">;</aside>
       <header className="w-full rounded-lg p-5 pl-4 mb-5 leading-loose dark:text-white">
         <div className="flex justify-between items-center">
-          <h1 className="text-4xl font-semibold mb-3">
-            Dev Blog
-          </h1>
+          <h1 className="text-4xl font-semibold mb-3">Dev Blog</h1>
         </div>
         <p className="dark:text-[rgb(141, 140, 142)]">
-          기록을 가까이
+          모든 것은 나에 대한 이해로부터 나온다
         </p>
       </header>
     </div>
--- components/skeleton/SkeletonPostCard.tsx
+++ components/skeleton/SkeletonPostCard.tsx
@@ -3,23 +3,20 @@ import React from "react";
 
 export default function SkeletonPostCard() {
   return (
-    <div className="flex flex-col w-80 h-52 gap-5">
-      <Skeleton className="rounded-lg w-full h-40 flex-shrink-0" />
-      <div className="flex flex-col gap-2 grow w-full min-w-0">
-        <div className="flex flex-col gap-3 mb-3">
-          <Skeleton className="h-6 w-60" />
-          <Skeleton className="h-2 w-40" />
-          <Skeleton className="h-2 w-32" />
-          <Skeleton className="h-2 w-44" />
-        </div>
-        <div className="grow flex items-end gap-2 mb-5">
+    <div className="flex flex-row gap-2 justify-between grow w-full min-w-0">
+      <div className="flex flex-col gap-3 mb-3">
+        <Skeleton className="h-6 w-80" />
+        <Skeleton className="h-4 w-40" />
+      </div>
+      <div className="space-y-2">
+        <div className="grow flex items-end gap-2">
           {Array(2)
             .fill(0)
             .map((_, index) => (
-              <Skeleton key={index} className="w-10 h-5 rounded-md" />
+              <Skeleton key={index} className="w-10 h-4 rounded-md" />
             ))}
         </div>
-        <Skeleton className="w-12 h-3" />
+        <Skeleton className="w-14 h-4" />
       </div>
     </div>
   );
