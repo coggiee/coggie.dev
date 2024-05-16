84ca28504499970c02d0e86aca259604a2e9577a
lunarmoon7 <zentechie7@gmail.com>
2024-05-16 12:02:13 +0900

    refactor: sitemap 재설정

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,22 +3,13 @@ import { getTotalPosts } from "../lib/hygraph";
 
 const BASE_URL = "https://coggie.dev";
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const { edges } = await getTotalPosts(50);
-  const posts = edges.map((post: any) => post.node);
-
-  const post_routes = posts.map((post: any) => ({
-    url: `${BASE_URL}/${post.id}`,
-    lastModified: post.date,
-    priority: 0.8,
-  }));
-
-  const routes = [""].map((route) => ({
-    url: `${BASE_URL}${route}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.7,
-  }));
-
-  return [...routes, ...post_routes];
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://coggie.dev",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
 }
